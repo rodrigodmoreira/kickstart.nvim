6c32d3de963b5f31b98ac2c25cab264744cad9bd
rodrigodmoreira <rodrigodmoreira.rdm@gmail.com>
2025-08-17 15:58:47 -0300

    Add pico-8 syntax highlight

--- init.lua
+++ init.lua
@@ -1005,6 +1005,7 @@ require('lazy').setup({
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
   { 'mg979/vim-visual-multi' },
+  { 'bakudankun/pico-8.vim' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
