93c8b26dc08cc984691080ca75ed9ef4155baeef
rodrigodmoreira <rodrigodmoreira.rdm@gmail.com>
2026-03-02 10:18:09 -0300

    disable lua auto styling

--- init.lua
+++ init.lua
@@ -712,7 +712,7 @@ require('lazy').setup({
         end
       end,
       formatters_by_ft = {
-        lua = { 'stylua' },
+        -- lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
